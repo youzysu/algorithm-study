a89e1c630d48779141ee635f3ba78193237cb187
jyj1111 <jyj202040@gmail.com>
2022-12-31 00:16:43 +0900

    feat: 2주차 크레인인형뽑기게임 주석 추가

--- jyj/크레인인형뽑기게임.js
+++ jyj/크레인인형뽑기게임.js
@@ -4,8 +4,9 @@ function solution(board, moves) {
   for (let i = 0; i < moves.length; i++) {
     for (let j = 0; j < board.length; j++) {
       if (board[j][moves[i] - 1] > 0) {
+        //moves[i]열에서 1행부터 탐색하여 j+1행에서 0값이 아니면 j행 moves[i]열 값을 꺼낸다.
         if (
-          basket.length > 0 &&
+          basket.length > 0 && //바구니가 비었지 않으면서  바구니 윗값과 게임화면에서 꺼낸값이 같은 경우
           board[j][moves[i] - 1] === basket[basket.length - 1]
         ) {
           basket.pop();
